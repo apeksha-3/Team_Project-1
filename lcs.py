418fe7cca98be37bd9a01669d5dfa3771ae516bc
apeksha-3 <apeksha3.sharma@gmail.com>
2025-10-29 22:44:50 +0530

    adding check checking similarity function

--- lcs.py
+++ lcs.py
@@ -9,3 +9,17 @@ def clean_text(text):
     text = re.sub(r'[^\w\s]', '', text)
     text = text.lower()
     return text.split()
+
+def are_similar(w1,w2):
+    if w1 == w2:
+        return True
+    
+    syns1 = wordnet.synset(w1)
+    syns2 = wordnet.synset(w2)
+
+    for s1 in syns1:
+        for s2 in syns2:
+            sim = s1.wup_similarity(s2)
+            if sim and sim > 0.75:
+                return True 
+    return False
